ce62dfeb74cfbadc031bab6703ede223bfbfd438
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-13 07:31:39 +0100

    Add getSnippet for dom service.

--- src/theme/components/ReadingBands/services/dom.ts
+++ src/theme/components/ReadingBands/services/dom.ts
@@ -1,5 +1,8 @@
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
+const SNIPPET_LENGTH_IDEAL: number = 30;
+const SNIPPET_TERMINATING_CHARACTERS: Array<string> = ['.', '!', '?', ':'];
+
 // TODO(dnguyen0304): Maybe refactor to reduce duplicated code with
 // getElementAll.
 export async function getElement(selector: string): Promise<Element> {
@@ -23,7 +26,7 @@ export async function getElement(selector: string): Promise<Element> {
             },
         );
     });
-}
+};
 
 // TODO(dnguyen0304): Maybe refactor to reduce duplicated code with getElement.
 export async function getElementAll(selector: string): Promise<Element[]> {
@@ -47,7 +50,7 @@ export async function getElementAll(selector: string): Promise<Element[]> {
             },
         );
     });
-}
+};
 
 export function getViewportHeight(): number {
     if (!ExecutionEnvironment.canUseDOM) {
@@ -57,4 +60,42 @@ export function getViewportHeight(): number {
         document.documentElement.clientHeight || 0,
         window.innerHeight || 0,
     );
-}
+};
+
+interface SubsetHTMLElement extends Pick<
+    HTMLElement,
+    'innerText' | 'textContent'
+> { };
+
+export function getSnippet(element: SubsetHTMLElement): string {
+    const text = element.innerText || element.textContent;
+    if (!text) {
+        return '';
+    }
+    const words = text.split(' ');
+    const snippetWords: Array<string> = [];
+
+    let wordCount = 0;
+    for (const word of words) {
+        if (wordCount + word.length > SNIPPET_LENGTH_IDEAL) {
+            if (!snippetWords.length) {
+                break;
+            }
+            const lastSnippetWord = snippetWords[snippetWords.length - 1];
+            // TODO(dnguyen0304): Investigate if this is possible.
+            if (!lastSnippetWord.length) {
+                break;
+            }
+            const lastSnippetCharacter =
+                lastSnippetWord[lastSnippetWord.length - 1];
+            if (SNIPPET_TERMINATING_CHARACTERS.includes(lastSnippetCharacter)) {
+                break;
+            }
+            snippetWords.push('...');
+            break
+        }
+        snippetWords.push(word);
+        wordCount += word.length;
+    }
+    return snippetWords.join(' ');
+};
