a42a6019d5b8ac5dabb70499d0a0a4a9ec270607
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-28 08:13:54 +0100

    Add background for Workbench/Header styles.

--- src/theme/components/Workbench/Header/index.tsx
+++ src/theme/components/Workbench/Header/index.tsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
+import Color from 'color';
 import * as React from 'react';
 import { CardText as CardTextStyles } from '../styles';
 import type { Sample as WorkbenchSample } from '../types';
@@ -8,16 +9,20 @@ import Cards from './Cards';
 
 export const HEIGHT: React.CSSProperties['height'] = '30%';
 
-const StyledBox = styled(Box)({
+const StyledBox = styled(Box)(({ theme }) => ({
     ...CardTextStyles,
     height: HEIGHT,
     display: 'flex',
     flexDirection: 'column',
+    background: `linear-gradient(
+        to bottom,
+        ${theme.palette.grey[600]} 0%,
+        ${Color(theme.palette.grey[800]).fade(0.3)} 50%)`,
     borderRadius: 'var(--border-radius) 0 var(--space-s) var(--space-s)',
     padding: 'var(--space-xs)',
     paddingTop: 'var(--space-l)',
     // backgroundColor: 'white',
-});
+}));
 
 interface Props {
     readonly keyedSamples: readonly (readonly [
