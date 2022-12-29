fbf221e0a6edf68f170dc9824591f679f4d50978
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-29 20:13:36 +0100

    Add Loading component.

---
+++ src/theme/components/Workbench/Loading/index.tsx
@@ -0,0 +1,31 @@
+import Box from '@mui/material/Box';
+import * as React from 'react';
+import styles from './styles.module.css';
+
+// TODO(dnguyen0304): Fix missing cohesion with styles.
+const ANIMATION_DURATION_MILLI = 5 * 1000;
+
+interface Props {
+    readonly setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function Loading(
+    {
+        setIsLoading,
+    }: Props
+): JSX.Element {
+    const timerId = React.useRef<number>();
+
+    React.useEffect(() => {
+        timerId.current = window.setTimeout(() => {
+            setIsLoading(false);
+        }, ANIMATION_DURATION_MILLI);
+        return () => window.clearTimeout(timerId.current);
+    }, []);
+
+    return (
+        <Box className={styles.loading_container}>
+            <Box className={styles.loading_bar} />
+        </Box>
+    );
+};
