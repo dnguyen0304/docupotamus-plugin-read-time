3a2bf3ca0ea5b4a2d4ef503eba6428894d23173e
Duy Nguyen <dnguyen0304@gmail.com>
2022-12-29 19:00:15 +0100

    Increase border-width for Card/Metric styles.

--- src/theme/components/Workbench/constants.ts
+++ src/theme/components/Workbench/constants.ts
@@ -1,6 +1,6 @@
 export const CONTENT_MARGIN_LEFT: string = 'var(--space-xs)';
 export const CARD_KEY_PREFIX: string = 'workbenchCard';
-export const METRIC_BORDER_WIDTH: React.CSSProperties['borderWidth'] = '0.5px';
+export const METRIC_BORDER_WIDTH: React.CSSProperties['borderWidth'] = '1px';
 export const RANK_ICON_WIDTH: React.CSSProperties['width'] = '24px';
 export const ACTIVE_INFO_ITEM_HEIGHT: string =
     `calc(2 * ${METRIC_BORDER_WIDTH} + ${RANK_ICON_WIDTH})`;
