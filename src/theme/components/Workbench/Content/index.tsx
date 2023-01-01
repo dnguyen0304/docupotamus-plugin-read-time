dc56dde85b7adfadd84bcd89f869d4db5e960146
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-01 10:35:43 -0700

    Add StyledBox for Workbench/Content component.

--- src/theme/components/Workbench/Content/index.tsx
+++ src/theme/components/Workbench/Content/index.tsx
@@ -1,15 +1,19 @@
+import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import * as React from 'react';
 import Card from '../Card';
 import { CARD_KEY_PREFIX } from '../constants';
 import type { Sample as WorkbenchSample } from '../types';
 
+const StyledBox = styled(Box)({
+    overflow: 'scroll',
+    marginBottom: 'var(--space-2xs)',
+});
+
 // TODO(dnguyen0304): Investigate migrating to use MUI List.
 //   See: https://mui.com/material-ui/react-list/
 const StyledOrderedList = styled('ol')({
-    overflow: 'scroll',
     margin: 0,
-    marginBottom: 'var(--space-2xs)',
     padding: 0,
 });
 
@@ -31,22 +35,24 @@ export default function Content(
     }: Props
 ): JSX.Element {
     return (
-        <StyledOrderedList>
-            {keyedSamples.map((preprocessed) => {
-                const [targetId, sample, rankCurr] = preprocessed;
-                const rankPrev = targetIdToPrevRank.get(targetId);
-                return (
-                    <Card
-                        key={`${CARD_KEY_PREFIX}-${targetId}`}
-                        targetId={targetId}
-                        details={sample.target.snippet}
-                        rankCurr={rankCurr}
-                        rankPrev={rankPrev}
-                        readTimeSecond={sample.runningTotal.readTimeSecond}
-                        showMinute={showMinute}
-                    />
-                );
-            })}
-        </StyledOrderedList>
+        <StyledBox>
+            <StyledOrderedList>
+                {keyedSamples.map((preprocessed) => {
+                    const [targetId, sample, rankCurr] = preprocessed;
+                    const rankPrev = targetIdToPrevRank.get(targetId);
+                    return (
+                        <Card
+                            key={`${CARD_KEY_PREFIX}-${targetId}`}
+                            targetId={targetId}
+                            details={sample.target.snippet}
+                            rankCurr={rankCurr}
+                            rankPrev={rankPrev}
+                            readTimeSecond={sample.runningTotal.readTimeSecond}
+                            showMinute={showMinute}
+                        />
+                    );
+                })}
+            </StyledOrderedList>
+        </StyledBox>
     );
 };
