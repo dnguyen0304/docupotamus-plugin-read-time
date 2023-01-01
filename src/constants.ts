9d3bc8f554cf78ad7ed38eb392da6f232895ffee
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-01 05:56:28 -0700

    Remove redundant DATA_ATTRIBUTE_TARGET_ID constant for useElement hook.

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
 export const RUNNING_TOTALS_UPDATE_RATE_MILLI: number = 5 * 1000;
-export const DATA_ATTRIBUTE_TARGET_ID: string = 'data-target-id';
--- src/theme/components/Workbench/hooks/useElement.tsx
+++ src/theme/components/Workbench/hooks/useElement.tsx
@@ -1,6 +1,5 @@
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import * as React from 'react';
-import { DATA_ATTRIBUTE_TARGET_ID } from '../../../../constants';
 import { getElement } from '../../../../services/dom';
 
 export default function useElement(
@@ -15,7 +14,7 @@ export default function useElement(
                 return;
             }
             const targetElement =
-                await getElement(`[${DATA_ATTRIBUTE_TARGET_ID}="${targetId}"]`);
+                await getElement(`[data-target-id="${targetId}"]`);
             setElement(targetElement);
         })();
         return cleanUp;
