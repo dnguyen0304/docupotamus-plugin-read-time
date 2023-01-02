afcfdf8445a88f483b0fb8bc81c999c4a9b4f66b
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-02 09:56:26 -0700

    Fix too-long-lines for docusaurus-plugin-read-time ambient.

--- src/docusaurus-plugin-read-time.d.ts
+++ src/docusaurus-plugin-read-time.d.ts
@@ -68,12 +68,12 @@ declare module '@docusaurus/plugin-read-time' {
         // Unique identifier that is human-readable.
         readonly friendlyKey: BandFriendlyKey;
 
-        // Top position as a percent of the viewport height. This range endpoint is
-        // inclusive: [top, bottom).
+        // Top position as a percent of the viewport height. This range endpoint
+        // is inclusive: [top, bottom).
         readonly topVh: number;
 
-        // Bottom position as a percent of the viewport height. This range endpoint
-        // is exclusive: [top, bottom).
+        // Bottom position as a percent of the viewport height. This range
+        // endpoint is exclusive: [top, bottom).
         readonly bottomVh: number;
 
         // Currently unused.
