31b4a6b7d7d87cd7c23684afe61df21e9cbe9a4c
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-06 07:29:51 -0700

    Fix undefined classList for Workbench/usePulse hook.

--- src/theme/components/Workbench/hooks/usePulse.tsx
+++ src/theme/components/Workbench/hooks/usePulse.tsx
@@ -7,15 +7,15 @@ export default function usePulse(
     const [isEnabled, setIsEnabled] = React.useState<boolean>(false);
 
     React.useEffect(() => {
-        element?.classList.remove(styles.target_container__pulse);
+        element?.classList?.remove(styles.target_container__pulse);
         if (isEnabled) {
             element?.scrollIntoView({
                 behavior: 'smooth',
                 block: 'center',
             });
-            element?.classList.add(styles.target_container__pulse);
+            element?.classList?.add(styles.target_container__pulse);
         }
-        return () => element?.classList.remove(styles.target_container__pulse);
+        return () => element?.classList?.remove(styles.target_container__pulse);
     }, [isEnabled]);
 
     return [isEnabled, setIsEnabled];
